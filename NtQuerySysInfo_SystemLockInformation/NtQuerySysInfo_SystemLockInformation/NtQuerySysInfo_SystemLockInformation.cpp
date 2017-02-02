0c872397ce347a5f0d3b47771dd280743761a7af
Sam Brown <samdanielbrown@gmail.com>
2017-02-02 23:00:20 +0000

    step1

---
+++ NtQuerySysInfo_SystemLockInformation/NtQuerySysInfo_SystemLockInformation/NtQuerySysInfo_SystemLockInformation.cpp
@@ -0,0 +1,62 @@
+// NtQuerySysInfo_SystemModuleInformation.cpp : Attempts to use the NtQuerySystemInformation to find the base addresses if loaded modules.
+//
+
+#include "stdafx.h"
+#include <windows.h>
+
+typedef struct _SYSTEM_LOCK {
+	PVOID	Address;
+	USHORT  Type;
+	USHORT  Reserved1;
+	ULONG	ExclusiveOwnerThreadId;
+	ULONG	ActiveCount;
+	ULONG	ContentionCount;
+	ULONG	Reserved2[2];
+	ULONG	NumberOfSharedWaiters;
+	ULONG	NumberOfExclusiveWaiters;
+} SYSTEM_LOCK, *PSYSTEM_LOCK;
+
+typedef struct SYSTEM_LOCK_INFORMATION {
+	ULONG              LocksCount;
+	SYSTEM_LOCK        Locks[1];
+} SYSTEM_LOCK_INFORMATION, *PSYSTEM_LOCK_INFORMATION;
+
+typedef enum _SYSTEM_INFORMATION_CLASS {
+	SystemLockInformation = 12
+} SYSTEM_INFORMATION_CLASS;
+
+typedef NTSTATUS(WINAPI *PNtQuerySystemInformation)(
+	__in SYSTEM_INFORMATION_CLASS SystemInformationClass,
+	__inout PVOID SystemInformation,
+	__in ULONG SystemInformationLength,
+	__out_opt PULONG ReturnLength
+	);
+
+int main()
+{
+	HMODULE ntdll = GetModuleHandle(TEXT("ntdll"));
+	PNtQuerySystemInformation query = (PNtQuerySystemInformation)GetProcAddress(ntdll, "NtQuerySystemInformation");
+	if (query == NULL) {
+		printf("GetProcAddress() failed.\n");
+		return 1;
+	}
+
+	PSYSTEM_LOCK_INFORMATION pLockInfo = NULL;
+	ULONG len = 20;
+	NTSTATUS status = (NTSTATUS)0xc0000004;
+
+	do {
+		len *= 2;
+		pLockInfo = (PSYSTEM_LOCK_INFORMATION)GlobalAlloc(GMEM_ZEROINIT, len);
+		status = query(SystemLockInformation, pLockInfo, len, &len);
+	} while (status == (NTSTATUS)0xc0000004);
+
+	for (int i = 0; i < pLockInfo->LocksCount; i++) {
+		PVOID lockAddress = pLockInfo->Locks[i].Address;
+		USHORT lockType = (USHORT)pLockInfo->Locks[i].Type;
+		printf("Lock Address 0x%X\t", lockAddress);
+		printf("Lock Type 0x%X\r\n", lockType);
+	}
+	return 0;
+}
+
---
+++ NtQuerySysInfo_SystemProcessInformation/NtQuerySysInfo_SystemProcessInformation/NtQuerySysInfo_SystemProcessInformation.cpp
@@ -0,0 +1,178 @@
+#include "stdafx.h"
+#include <windows.h>
+
+typedef LONG       KPRIORITY;
+typedef struct _CLIENT_ID {
+	DWORD          UniqueProcess;
+	DWORD          UniqueThread;
+} CLIENT_ID;
+
+typedef struct _UNICODE_STRING {
+	USHORT         Length;
+	USHORT         MaximumLength;
+	PWSTR          Buffer;
+} UNICODE_STRING;
+
+
+//from http://boinc.berkeley.edu/android-boinc/boinc/lib/diagnostics_win.h
+typedef struct _VM_COUNTERS {
+#ifdef _WIN64
+	// the following was inferred by painful reverse engineering
+	SIZE_T		   PeakVirtualSize;	// not actually
+	SIZE_T         PageFaultCount;
+	SIZE_T         PeakWorkingSetSize;
+	SIZE_T         WorkingSetSize;
+	SIZE_T         QuotaPeakPagedPoolUsage;
+	SIZE_T         QuotaPagedPoolUsage;
+	SIZE_T         QuotaPeakNonPagedPoolUsage;
+	SIZE_T         QuotaNonPagedPoolUsage;
+	SIZE_T         PagefileUsage;
+	SIZE_T         PeakPagefileUsage;
+	SIZE_T         VirtualSize;		// not actually
+#else
+	SIZE_T         PeakVirtualSize;
+	SIZE_T         VirtualSize;
+	ULONG          PageFaultCount;
+	SIZE_T         PeakWorkingSetSize;
+	SIZE_T         WorkingSetSize;
+	SIZE_T         QuotaPeakPagedPoolUsage;
+	SIZE_T         QuotaPagedPoolUsage;
+	SIZE_T         QuotaPeakNonPagedPoolUsage;
+	SIZE_T         QuotaNonPagedPoolUsage;
+	SIZE_T         PagefileUsage;
+	SIZE_T         PeakPagefileUsage;
+#endif
+} VM_COUNTERS;
+
+typedef enum _KWAIT_REASON
+{
+	Executive = 0,
+	FreePage = 1,
+	PageIn = 2,
+	PoolAllocation = 3,
+	DelayExecution = 4,
+	Suspended = 5,
+	UserRequest = 6,
+	WrExecutive = 7,
+	WrFreePage = 8,
+	WrPageIn = 9,
+	WrPoolAllocation = 10,
+	WrDelayExecution = 11,
+	WrSuspended = 12,
+	WrUserRequest = 13,
+	WrEventPair = 14,
+	WrQueue = 15,
+	WrLpcReceive = 16,
+	WrLpcReply = 17,
+	WrVirtualMemory = 18,
+	WrPageOut = 19,
+	WrRendezvous = 20,
+	Spare2 = 21,
+	Spare3 = 22,
+	Spare4 = 23,
+	Spare5 = 24,
+	WrCalloutStack = 25,
+	WrKernel = 26,
+	WrResource = 27,
+	WrPushLock = 28,
+	WrMutex = 29,
+	WrQuantumEnd = 30,
+	WrDispatchInt = 31,
+	WrPreempted = 32,
+	WrYieldExecution = 33,
+	WrFastMutex = 34,
+	WrGuardedMutex = 35,
+	WrRundown = 36,
+	MaximumWaitReason = 37
+} KWAIT_REASON;
+
+
+typedef struct _SYSTEM_THREAD_INFORMATION{
+	LARGE_INTEGER KernelTime;
+	LARGE_INTEGER UserTime;
+	LARGE_INTEGER CreateTime;
+	ULONG WaitTime;
+	PVOID StartAddress;
+	CLIENT_ID ClientId;
+	KPRIORITY Priority;
+	LONG BasePriority;
+	ULONG ContextSwitches;
+	ULONG ThreadState;
+	KWAIT_REASON WaitReason;
+} SYSTEM_THREAD_INFORMATION, *PSYSTEM_THREAD_INFORMATION;
+
+typedef struct _SYSTEM_EXTENDED_THREAD_INFORMATION
+{
+	SYSTEM_THREAD_INFORMATION ThreadInfo;
+	PVOID StackBase;
+	PVOID StackLimit;
+	PVOID Win32StartAddress;
+	PVOID TebAddress;
+	ULONG Reserved1;
+	ULONG Reserved2;
+	ULONG Reserved3;
+} SYSTEM_EXTENDED_THREAD_INFORMATION, *
+PSYSTEM_EXTENDED_THREAD_INFORMATION;
+
+typedef struct _SYSTEM_EXTENDED_PROCESS_INFORMATION
+{
+	ULONG NextEntryOffset;
+	ULONG NumberOfThreads;
+	LARGE_INTEGER SpareLi1;
+	LARGE_INTEGER SpareLi2;
+	LARGE_INTEGER SpareLi3;
+	LARGE_INTEGER CreateTime;
+	LARGE_INTEGER UserTime;
+	LARGE_INTEGER KernelTime;
+	UNICODE_STRING ImageName;
+	KPRIORITY BasePriority;
+	ULONG UniqueProcessId;
+	ULONG InheritedFromUniqueProcessId;
+	ULONG HandleCount;
+	ULONG SessionId;
+	PVOID PageDirectoryBase;
+	VM_COUNTERS VirtualMemoryCounters;
+	SIZE_T PrivatePageCount;
+	IO_COUNTERS IoCounters;
+	SYSTEM_EXTENDED_THREAD_INFORMATION Threads[1];
+} SYSTEM_EXTENDED_PROCESS_INFORMATION, *PSYSTEM_EXTENDED_PROCESS_INFORMATION;
+
+
+typedef enum _SYSTEM_INFORMATION_CLASS {
+	SystemSessionProcessInformation = 57
+} SYSTEM_INFORMATION_CLASS;
+
+typedef NTSTATUS(WINAPI *PNtQuerySystemInformation)(
+	__in SYSTEM_INFORMATION_CLASS SystemInformationClass,
+	__inout PVOID SystemInformation,
+	__in ULONG SystemInformationLength,
+	__out_opt PULONG ReturnLength
+	);
+
+int main()
+{
+	HMODULE ntdll = GetModuleHandle(TEXT("ntdll"));
+	PNtQuerySystemInformation query = (PNtQuerySystemInformation)GetProcAddress(ntdll, "NtQuerySystemInformation");
+	if (query == NULL) {
+		printf("GetProcAddress() failed.\n");
+		return 1;
+	}
+	ULONG len = 20;
+	NTSTATUS status = NULL;
+	PSYSTEM_EXTENDED_PROCESS_INFORMATION pProcessInfo = NULL;
+	do {
+		len *= 2;	
+		pProcessInfo = (PSYSTEM_EXTENDED_PROCESS_INFORMATION)GlobalAlloc(GMEM_ZEROINIT, len);
+		status = query(SystemSessionProcessInformation, pProcessInfo, len, &len);
+	} while (status == (NTSTATUS)0xc0000004);
+
+	for (unsigned int i = 0; i < pProcessInfo->NumberOfThreads; i++) {
+		PVOID stackBase = pProcessInfo->Threads[i].StackBase;
+		PVOID stackLimit = pProcessInfo->Threads[i].StackLimit;
+		printf("Stack base 0x%X\t", stackBase);
+		printf("Stack limit 0x%X\r\n", stackLimit);
+	}
+	return 0;
+}
+
+
