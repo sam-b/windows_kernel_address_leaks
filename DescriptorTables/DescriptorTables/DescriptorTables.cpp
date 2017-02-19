b271f76b0308c3fa736f30d46d65d91108be2071
Sam Brown <samdanielbrown@gmail.com>
2017-02-19 22:59:13 +0000

    brain fart...

--- DescriptorTables/DescriptorTables/DescriptorTables.cpp
+++ DescriptorTables/DescriptorTables/DescriptorTables.cpp
@@ -12,28 +12,28 @@ int main()
 #ifdef _WIN64
 	unsigned char idtr[10] = { 0 };
 	__sidt(idtr);
-	unsigned long long idtrBase = (unsigned long long)idtr[0] << 56 |
-		(unsigned long long)idtr[1] << 48 |
-		(unsigned long long)idtr[2] << 40 |
-		(unsigned long long)idtr[3] << 32 |
-		(unsigned long long)idtr[4] << 24 |
-		(unsigned long long)idtr[5] << 16 |
-		(unsigned long long)idtr[6] << 8 |
-		(unsigned long long)idtr[7];
-	unsigned short idtrLimit = (unsigned int)idtr[8] << 8 |
-		(unsigned int)idtr[9];
+	unsigned long long idtrBase = (unsigned long long)idtr[9] << 56 |
+		(unsigned long long)idtr[8] << 48 |
+		(unsigned long long)idtr[7] << 40 |
+		(unsigned long long)idtr[6] << 32 |
+		(unsigned long long)idtr[5] << 24 |
+		(unsigned long long)idtr[4] << 16 |
+		(unsigned long long)idtr[3] << 8 |
+		(unsigned long long)idtr[2];
+	unsigned short idtrLimit = (unsigned int)idtr[1] << 8 |
+		(unsigned int)idtr[0];
 	printf("Interrupt Descriptor Table Register base: 0x%llx, limit: 0x%X\r\n", idtrBase, idtrLimit);
 #else
 	unsigned char idtr[6] = { 0 };
 	__asm {
 		sidt idtr;
 	}
-	unsigned int idtrBase = (unsigned int)idtr[0] << 24 |
-		(unsigned int)idtr[1] << 16 |
-		(unsigned int)idtr[2] << 8 |
-		(unsigned int)idtr[3];
-	unsigned short idtrLimit = (unsigned int)idtr[4] << 8 |
-		(unsigned int)idtr[5];
+	unsigned int idtrBase = (unsigned int)idtr[5] << 24 |
+		(unsigned int)idtr[4] << 16 |
+		(unsigned int)idtr[3] << 8 |
+		(unsigned int)idtr[2];
+	unsigned short idtrLimit = (unsigned int)idtr[1] << 8 |
+		(unsigned int)idtr[0];
 	printf("Interrupt Descriptor Table Register base: 0x%X, limit: 0x%X\r\n", idtrBase, idtrLimit);
 #endif
 
@@ -42,28 +42,28 @@ int main()
 #ifdef _WIN64
 	unsigned char gdtr[10] = { 0 };
 	_sgdt(gdtr);
-	unsigned long long gdtrBase = (unsigned long long)gdtr[0] << 56 |
-		(unsigned long long)gdtr[1] << 48 |
-		(unsigned long long)gdtr[2] << 40 |
-		(unsigned long long)gdtr[3] << 32 |
-		(unsigned long long)gdtr[4] << 24 |
-		(unsigned long long)gdtr[5] << 16 |
-		(unsigned long long)gdtr[6] << 8 |
-		(unsigned long long)gdtr[7];
-	unsigned short gdtrLimit = (unsigned int)gdtr[8] << 8 |
-		(unsigned int)gdtr[9];
+	unsigned long long gdtrBase = (unsigned long long)gdtr[9] << 56 |
+		(unsigned long long)gdtr[8] << 48 |
+		(unsigned long long)gdtr[7] << 40 |
+		(unsigned long long)gdtr[6] << 32 |
+		(unsigned long long)gdtr[5] << 24 |
+		(unsigned long long)gdtr[4] << 16 |
+		(unsigned long long)gdtr[3] << 8 |
+		(unsigned long long)gdtr[2];
+	unsigned short gdtrLimit = (unsigned int)gdtr[1] << 8 |
+		(unsigned int)gdtr[0];
 	printf("Global Descriptor Table Register base: 0x%llx, limit: 0x%X\r\n", gdtrBase, gdtrLimit);
 #else
 	unsigned char gdtr[6] = { 0 };
 	__asm {
 		sgdt gdtr;
 	}
-	unsigned int gdtrBase = (unsigned int)gdtr[0] << 24 |
-		(unsigned int)gdtr[1] << 16 |
-		(unsigned int)gdtr[2] << 8 |
-		(unsigned int)gdtr[3];
-	unsigned short gdtrLimit = (unsigned int)gdtr[4] << 8 |
-		(unsigned int)gdtr[5];
+	unsigned int gdtrBase = (unsigned int)gdtr[5] << 24 |
+		(unsigned int)gdtr[4] << 16 |
+		(unsigned int)gdtr[3] << 8 |
+		(unsigned int)gdtr[2];
+	unsigned short gdtrLimit = (unsigned int)gdtr[1] << 8 |
+		(unsigned int)gdtr[0];
 	printf("Global Descriptor Table Register base: 0x%X, limit: 0x%X\r\n", gdtrBase, gdtrLimit);
 #endif
 
